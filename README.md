8521d835a8972f043c432eae8d05eb2c69cb2b2b
Damian Nowok <damian.nowok@gmail.com>
2021-03-31 23:11:34 +0200

    Update for FW and documentation

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AutoSwitch
 Device that allows to automatically switch on secondary outlet when current is detected on primary outlet. Based on ATtiny85 microcontroller and ACS712 current sensor.
+
+Photo of the device
+----------------------
+3D printed plastic cover. PLA material.
+![Plastic cover](/pics/20210327_104129.jpg?raw=true "Plastic cover")
+
+Schematic
+---------
+![Schematic](/docs/schematic.PNG?raw=true "Schematic")
--- src/outlet_firmware/outlet_firmware.ino
+++ src/outlet_firmware/outlet_firmware.ino
@@ -1,37 +1,21 @@
-#include <math.h>
+#include "ACS712.h"
 
-#define ADC_DELTA 5 
+#define ADC_DELTA 200 
 #define ADC1 1
-#define SAMPLES 100
 
 int initial_voltage = 0;
-
-int read_voltage(int pin) {
-  int retry = SAMPLES;
-  int voltage[SAMPLES];
-  
-  for(int i=0; i < SAMPLES; i++) {
-    voltage[i] = analogRead(pin);
-    delay(1);
-  }
-
-  long sum = 0;
-  for(int i=0; i < SAMPLES; i++) {
-    sum += pow(voltage[i], 2);
-  }
-  
-  return (int)sqrt(sum/SAMPLES);
-}
+ACS712  ACS(ADC1, 5.0, 1023, 66);
 
 void setup() {
  //Initialisierung der digitalen PINs
  pinMode(PB0, OUTPUT); //Relay
  pinMode(PB1, INPUT); //Button
- initial_voltage = read_voltage(ADC1);
+ ACS.autoMidPoint();
+ initial_voltage = ACS.mA_AC();
 }
 
 void loop() {
- int voltage = read_voltage(ADC1);
+ int voltage = ACS.mA_AC();
  int button_on_off = digitalRead(PB1);
 
 if (button_on_off == HIGH || voltage > initial_voltage + ADC_DELTA || voltage < initial_voltage - ADC_DELTA) {
